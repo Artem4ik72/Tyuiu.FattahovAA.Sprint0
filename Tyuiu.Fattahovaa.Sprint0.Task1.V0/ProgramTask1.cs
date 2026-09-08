af9701929fa69e4a60f2e351945227d110995f59
Artem <Artem@DESKTOP-FMG194D>
2026-09-08 16:34:03 +0500

    Task1

---
+++ Tyuiu.Fattahovaa.Sprint0.Task1.V0/ProgramTask1.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Hello, World!");
+string name = "Артём";
+int age = 17;
+Console.WriteLine($"Привет, меня зовут {name} и мне {age} лет. Я студент первого курса");
+Console.ReadKey();
